8e856948effd9cbe6c4708e40dcc506f8a532bb3
C. Sean Burns <sean.csb@gmail.com>
2018-10-06 16:42:15 -0400

    continued analysis

--- searches.R
+++ searches.R
@@ -15,7 +15,7 @@ colnames(searches) <- c("pubmed", "proquest", "ebscohost", "wos", "ovid")
 rm(s01, s02, s03, s04, s05, s06, s07, s08, s09, s10, s11, s12, s13, s14, s15,
    s16, s17, s18, s19, s20, s21, s22, s23, s24, s25, s26, s27, s28, s29)
 
-# usage: modz (row, startcol, endcol)
+# calculate modified z-score *m_i*
 # function based on pubmed count and not median as center
 modz <- function(searches, x) {
   s     <- as.numeric(searches[x, 1:5])
@@ -43,17 +43,6 @@ colnames(modz) <- c("pubmed", "proquest", "ebscohost", "wos", "ovid")
 # perfectly or nearly perfectly correlated
 modz[c(16, 17, 19, 20), ] <- c(0, 0, 0, 0)
 
-# Create a correlation matrix plot
-# Remove pubmed since those are zeroed out
-library("corrplot")
-modcor <- modz[, -1]
-modcor <- cor(modcor, method = "spearman")
-png("plots/corrplot.png", height = 9, width = 9, units = "in", res = 300)
-corrplot.mixed(modcor, upper = "ellipse", number.cex = 1.0,
-               addgrid.col = "grey", lower.col = "black",
-               upper.col = grey.colors(100))
-dev.off()
-
 # Create 
 # gridExtra is for a multiplot
 library("ggplot2")
@@ -76,3 +65,69 @@ png("plots/multi-point-plot.png", height = 9, width = 9,
     units = "in", res = 300)
 grid.arrange(mz1, mz2, mz3, mz4, ncol = 2, nrow = 2)
 dev.off()
+
+# View of the scores that are greater or lesser than 1 median
+# standard deviations from the PubMed scores:
+# then, total how many scores are plus or minus 1 modified deviations
+
+pqoutliers <- function(modz, proquest) {
+  pqout1 <- cbind(as.matrix(modz$proquest),
+                (modz$proquest >= 1 | modz$proquest <= -1))
+  pqsum1 <- sum(pqout1[, 2])
+  pqout2 <- cbind(as.matrix(modz$proquest),
+                (modz$proquest >= 2 | modz$proquest <= -2))
+  pqsum2 <- sum(pqout2[, 2])
+  pqsum1 <- pqsum1 - pqsum2
+  cat("result: more than one, more than two\n")
+  return(c(pqsum1, pqsum2))
+}
+
+pqoutliers(modz, proquest)
+
+eboutliers <- function(modz, ebscohost) {
+  ebout1 <- cbind(as.matrix(modz$ebscohost),
+                (modz$ebscohost >= 1 | modz$ebscohost <= -1))
+  ebsum1 <- sum(ebout1[, 2])
+  ebout2 <- cbind(as.matrix(modz$ebscohost),
+                (modz$ebscohost >= 2 | modz$ebscohost <= -2))
+  ebsum2 <- sum(ebout2[, 2])
+  ebsum1 <- ebsum1 - ebsum2
+  cat("result: more than one, more than two\n")
+  return(c(ebsum1, ebsum2))
+}
+
+eboutliers(modz, ebscohost)
+
+wosoutliers <- function(modz, wos) {
+  wosout1 <- cbind(as.matrix(modz$wos),
+                (modz$wos >= 1 | modz$wos <= -1))
+  wossum1 <- sum(wosout1[, 2])
+  wosout2 <- cbind(as.matrix(modz$wos),
+                (modz$wos >= 2 | modz$wos <= -2))
+  wossum2 <- sum(wosout2[, 2])
+  wossum1 <- wossum1 - wossum2
+  cat("result: more than one, more than two\n")
+  return(c(wossum1, wossum2))
+}
+
+wosoutliers(modz, wos)
+
+ovidoutliers <- function(modz, ovid) {
+  ovidout1 <- cbind(as.matrix(modz$ovid),
+                (modz$ovid >= 1 | modz$ovid <= -1))
+  ovidsum1 <- sum(ovidout1[, 2])
+  ovidout2 <- cbind(as.matrix(modz$ovid),
+                (modz$ovid >= 2 | modz$ovid <= -2))
+  ovidsum2 <- sum(ovidout2[, 2])
+  ovidsum1 <- ovidsum1 - ovidsum2
+  cat("result: more than one, more than two\n")
+  return(c(ovidsum1, ovidsum2))
+}
+
+ovidoutliers(modz, ovid)
+
+library("xtable")
+modztable <- as.data.frame(round(modz[, -1], 3))
+
+# convert to odt with pandoc
+print(xtable(modztable), file = "docs/table1.tex")
